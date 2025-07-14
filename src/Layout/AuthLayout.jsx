6b9fd1bad4ef9c6fc4cd6febb070b1a590fd2ded
Muhammad Ubaid ur Rehman <uurrehman735@gmail.com>
2025-07-14 10:51:03 +0500

    Deleted AuthLayout.jsx due to no real usage

--- src/Layout/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-// const AuthLayout = ({ children }) => {
-//   return (
-//     <div className="bg-gray-800 p-8 rounded-lg shadow-xl max-w-md w-full border border-gray-700">
-//       {children}
-//     </div>
-//   );
-// };
-
-// export default AuthLayout;
